d9b573cc57552cd1514d764d441ce2491b7e5038
eneylton <eneylton@hotmail.com>
2019-06-07 23:54:24 -0300

    Insert - fileUpload no Banco mydsql

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,39 +2,36 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { Camera } from '@ionic-native/camera';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { GaleriaServiceProvider } from '../providers/galeria-service/galeria-service';
+import { ImageUtilProvider } from '../providers/galeria-service/image-util';
 
 @NgModule({
   declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    MyApp
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    MyApp
   ],
   providers: [
     StatusBar,
+    Camera,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ImageUtilProvider,
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    GaleriaServiceProvider
   ]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/config/api.config.ts
@@ -0,0 +1,4 @@
+export const API_CONFIG = {
+    baseUrl: "http://localhost:8082",
+    upload:  "http://localhost:8082/fotos"
+}
